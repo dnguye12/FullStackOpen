e1d1e0c7dfd651c01b057d62f601faea3fb8301a
dnguye12 <duchuyng051@gmail.com>
2023-05-19 08:21:46 +0200

    rename

--- part1/src/Total.js
+++
@@ -1,9 +0,0 @@
-const Total = (props) => {
-    const sum = props.exercises.reduce((helper, a) => helper + a, 0);
-    return (
-        
-        <p>Number of exercises {sum}</p>
-    )
-}
-
-export default Total
