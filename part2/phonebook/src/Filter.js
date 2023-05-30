72b74bebc4732db12ff9fb57ee55ee5b5d53ca68
dnguye12 <duchuyng051@gmail.com>
2023-05-30 10:42:23 +0200

    exercicese 3.13-3.14

--- part2/phonebook/src/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({searchName, handleSearchNameChange}) => {
-    return (
-        <div>
-            Filter shown with <input value={searchName} onChange={handleSearchNameChange}></input>
-        </div>
-    )
-}
-
-export default Filter
--- part2/phonebook/src/Persons.js
+++
@@ -1,9 +0,0 @@
-const Persons = ({personsToShow, deleto}) => {
-    return (
-        <div>
-        {personsToShow.map(p => <p>{p.name} : {p.number} <button onClick={() => deleto(p.id)}>Delete person</button></p>)}
-      </div>
-    )
-}
-
-export default Persons;
