10359d4386989cc8005d2409ec082d7f67cab758
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-03-28 11:15:26 +0100

    part 3 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -64,13 +64,13 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', postMorgan, (request, response) => {
+app.post('/api/persons', postMorgan, (request, response, next) => {
     const body = request.body
     if (!body.name || !body.number) {
         response.status(404).json({
@@ -92,27 +92,23 @@ app.post('/api/persons', postMorgan, (request, response) => {
             persons = persons.concat(savedPerson)
             response.json(savedPerson)
         })
+            .catch(error => next(error))
     }
 })
 
 app.put('/api/persons/:id', (request, response) => {
-    const body = request.body
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
-
-    Person.findByIdAndUpdate(request.params.id, person, {new : true})
-    .then(updatedPerson => {
-        response.json(updatedPerson)
-        persons = persons.map(p => p.id === updatedPerson.id ? updatedPerson : p)
-    })
-    .catch(error => next(error))
+    const {name, number} = request.body
+    
+    Person.findByIdAndUpdate(request.params.id, {name, number}, { new: true, runValidators: true, context: 'query' })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+            persons = persons.map(p => p.id === updatedPerson.id ? updatedPerson : p)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -122,6 +118,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
