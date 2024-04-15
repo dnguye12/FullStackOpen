585f0e69a6d5cab46a07c3d1e9d680a9b13e1d24
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-04-15 09:04:29 +0200

    part 6.9 done

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+    const anecdotes = useSelector(state => {
+        if(state.filter === '') {
+            return state.anecdotes.sort((a, b) => b.votes - a.votes)
+        }else {
+            return state.anecdotes.filter((a) => a.content.toLowerCase().includes(state.filter.toLowerCase())).sort((a, b) => b.votes - a.votes)
+        }
+        
+    })
     const dispatch = useDispatch()
 
     const vote = (id) => {
