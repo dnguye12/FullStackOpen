e81ec0eed41abf0ecf967623ccadda9a25d27383
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-05-16 22:44:10 +0200

    part 9.6-9.7

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -75,3 +75,5 @@ try {
     }
     console.log(errorMessage);
 }
+
+export default calculateExercises;
--- part9/index.ts
+++ part9/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import qs from 'qs';
 import calculateBmi from './bmiCalculator';
+import calculateExercises from './exerciseCalculator';
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.get('/bmi', (_req, res) => {
     if (isNaN(height) || isNaN(weight)) {
         res.send({
             error: "malformatted parameters"
-          });
+        });
     }
 
     res.send({
@@ -25,9 +26,22 @@ app.get('/bmi', (_req, res) => {
         'height': height,
         'bmi': calculateBmi(weight, height)
     })
-
-
 });
+
+app.get('exercises', (_req, res) => {
+    const query = qs.parse(_req.query as unknown as string);
+
+    const daily_exercises:number[] = query.daily_exercises ? (Array.isArray(query.daily_exercises) ? query.daily_exercises.map(Number) : [Number(query.daily_exercises)]) : [];
+    const target:number = Number(query.target);
+
+    if (!Array.isArray(daily_exercises) || isNaN(target)) {
+        res.send({
+            error: "parameters missing"
+        });
+    }
+    daily_exercises.push(target);
+    res.send(calculateExercises(daily_exercises));
+})
 
 const PORT = 3003;
 
